b1007a6a1deae492d881b73c8ecfd875272ddff0
tkt1tm <takuto.itami.l@gmail.com>
2025-05-20 14:40:39 +0900

    ログインする機能の作成
    AdministratorServiceクラスに管理者ログイン処理を作成
    
    ログイン時に成功/失敗で遷移先を分けるメソッドの作成
    
    ログイン失敗時にエラーメッセージが出るようにビューを編集
    
    ログインするメソッドのJavDocを修正

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -4,9 +4,11 @@ import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,9 @@ public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
 
+    @Autowired
+    private HttpSession session;
+
     /**
      * ログイン画面を表示させます.
      *
@@ -32,6 +37,27 @@ public class AdministratorController {
     @GetMapping("/")
     public String toLogin(LoginForm form) {
         return "administrator/login";
+    }
+
+    /**
+     * 管理者のログイン処理を行います.
+     *
+     * @param form 入力されたメールアドレスとパスワードが格納されている
+     * @param model リクエストパラメータを格納(エラーメッセージ用)
+     * @return ログイン成功時は従業員一覧へフォワード/失敗時はログイン画面にリダイレクト
+     */
+    @PostMapping("/login")
+    public String login(LoginForm form, Model model) {
+        Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+
+        if (administrator == null) {
+            model.addAttribute("errorMsg", "メールアドレスまたはパスワードが不正です。");
+            return "administrator/login";
+        }
+
+        session.setAttribute("administratorName", administrator.getName());
+
+        return "redirect:/employee/showList";
     }
 
     /**
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -27,4 +27,14 @@ public class AdministratorService {
         administratorRepository.insert(administrator);
     }
 
+    /**
+     * ログイン処理を行います.
+     *
+     * @param mailAddress 入力されたメールアドレス
+     * @param password 入力されたパスワード
+     * @return 入力した値を使った検索で取得した管理者情報
+     */
+    public Administrator login(String mailAddress, String password) {
+        return administratorRepository.findByMailAddressAndPassword(mailAddress, password);
+    }
 }
